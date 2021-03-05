84b23c734c61dde2cb488ff92c08cfa7d54e8eca
dptindugan <dptindugan@gmail.com>
2021-03-06 01:08:06 +0800

    updated console.logs

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ const arrangeByValue = require('./arrangeByValue');
 
 const shuffledDeck = shuffle(cards);
 
-// arrangeByValue(shuffle(cards))
+console.log('shuffled deck', shuffledDeck);
+console.log('==============');
+
+console.log('arrange by suit');
+console.log(arrageBySuit(shuffle(cards)));
+console.log('==============');
+
+console.log('arrange by value')
+console.log('ascending', arrangeByValue(shuffle(cards)));
+console.log('descending', arrangeByValue(shuffle(cards), 'descending'));
+console.log('==============');
+
+console.log('deal cards');
 console.log(dealCard(shuffledDeck), shuffledDeck.length);
 console.log(dealCard(shuffledDeck), shuffledDeck.length);
 console.log(dealCard(shuffledDeck), shuffledDeck.length);
 console.log(dealCard(shuffledDeck), shuffledDeck.length);
 console.log(dealCard(shuffledDeck), shuffledDeck.length);
 console.log(dealCard(shuffledDeck), shuffledDeck.length);
-// console.log(arrageBySuit(shuffle(cards)))
+console.log('==============');
